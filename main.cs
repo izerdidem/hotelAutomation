bf8d5422911bfaa7318b30a9f314dadc47cd8a11
izerdidem <39744109+izerdidem@users.noreply.github.com>
2018-09-27 15:32:16 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelAutomation
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmCustomerRecord custRecord = new frmCustomerRecord();
+            custRecord.Show();
+            
+        }
+    }
+}
